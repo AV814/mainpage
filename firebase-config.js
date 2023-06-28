a4ae50939476b1b67517b3e9f39512d63f01927e
AV814 <137857152+AV814@users.noreply.github.com>
2023-06-27 22:41:26 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyByOJEl2Q5NX5OsuZ2yobTUXlElx0VSooY",
+    authDomain: "textpage1-chair.firebaseapp.com",
+    databaseURL: "https://textpage1-chair-default-rtdb.firebaseio.com",
+    projectId: "textpage1-chair",
+    storageBucket: "textpage1-chair.appspot.com",
+    messagingSenderId: "498007060580",
+    appId: "1:498007060580:web:a4fe4e2dc5d2bfb68f2f71",
+    measurementId: "G-Y23K3YNV76"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
